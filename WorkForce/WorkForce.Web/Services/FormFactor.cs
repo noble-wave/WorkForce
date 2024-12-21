d4711bd55ce17661790b244e78f36f8c03ce84b6
Noble Wave <noble-wave@outlook.com>
2024-12-21 13:46:54 +0530

    [SA]: Repo Init

---
+++ WorkForce/WorkForce.Web/Services/FormFactor.cs
@@ -0,0 +1,16 @@
+using WorkForce.Shared.Services;
+
+namespace WorkForce.Web.Services;
+
+public class FormFactor : IFormFactor
+{
+    public string GetFormFactor()
+    {
+        return "Web";
+    }
+
+    public string GetPlatform()
+    {
+        return Environment.OSVersion.ToString();
+    }
+}
---
+++ WorkForce/WorkForce/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkForce;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
